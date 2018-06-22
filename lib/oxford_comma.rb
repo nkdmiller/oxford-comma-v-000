2bf3203181bcc945c0e76b0c7e7334ff4baec007
Nate Miller <natekmiller96@gmail.com>
2018-06-22 20:42:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
 def oxford_comma(array)
-
+  counter = array.length
+  comma_array = []
+  if counter == 1
+    return array.join
+  elsif counter == 2
+    binary_array = []
+    array.each do |x|
+      if counter == 2
+        binary_array << "#{x}"
+        counter -= 1
+      else
+        binary_array << " and #{x}"
+        return binary_array.join
+      end
+    end
+  else
+    array.each do |x|
+      if counter != 1
+        comma_array << "#{x}, "
+        counter -= 1
+      else
+        comma_array << "and #{x}"
+      end
+    end
+    return comma_array.join
+  end
 end
