57e37be7fc19aea5ffbda942dfd76be053371c20
Nate Miller <natekmiller96@gmail.com>
2018-06-22 20:30:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  counter = array.length
+  comma_array = ""
+  array.each do |x|
+    if counter != 0
+      comma_array << "#{x}, "
+      counter -= 1
+    else
+      comma_array << "and #{x}."
+
+
+
 
 end
